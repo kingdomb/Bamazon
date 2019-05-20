da973f72494f3956bc3279e224611b9e2be0273f
King Conch <kbmajor80@gmail.com>
2019-05-20 00:09:47 -0400

    WIP:Created preliminary functions for db query.

--- Customer.js
+++ Customer.js
@@ -17,9 +17,94 @@ var connection = mysql.createConnection({
     //connection.end();
   });
 
-  // console.table();
-connection.query('SELECT * FROM inventory', (err, data)=>{
-  if (err) throw err;
-  console.table(data);
-});
-  
+//   // console.table();
+// connection.query('SELECT * FROM inventory', (err, data)=>{
+//   if (err) throw err;
+//   console.table(data);
+// });
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What is the ID of the item that you would like to purchase?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song",
+        "exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Find songs by artist":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+          
+      case "exit":
+        connection.end();
+        break;
+      }
+    });
+}
+
+
+function rangeSearch() {
+  inquirer
+    .prompt([
+      {
+        name: "start",
+        type: "input",
+        message: "Enter starting position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "end",
+        type: "input",
+        message: "Enter ending position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+      connection.query(query, [answer.start, answer.end], function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Position: " +
+              res[i].position +
+              " || Song: " +
+              res[i].song +
+              " || Artist: " +
+              res[i].artist +
+              " || Year: " +
+              res[i].year
+          );
+        }
+        runSearch();
+      });
+    });
+}
